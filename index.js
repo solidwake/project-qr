5f36b560761303288c0895cedf0f2ad7f0e68fc1
Idris Westbrook <westbrook.idris@gmail.com>
2022-10-17 12:06:17 -0400

    add image download

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const onGenerateSubmit = (e) => {
         setTimeout(() => {
             hideSpinner();
             generateQRCode(url, size);
+
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src
+                createSaveBtn(saveUrl)
+            }, 50)
         }, 1000)
     }
 }
@@ -36,6 +41,18 @@ const hideSpinner = () => {
 
 const clearUI = () => {
     qr.innerHTML = '';
+    const saveLink = document.getElementById('save-link')
+    if (saveLink) saveLink.remove();
+}
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a')
+    link.id = 'save-link'
+    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5'
+    link.href = saveUrl;
+    link.download = 'qrcode'
+    link.innerHTML = 'Save Image'
+    document.getElementById('generated').appendChild(link)
 }
 
 hideSpinner();
